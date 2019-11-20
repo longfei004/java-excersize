d452f5ca02eb1c0e49189501860762c988bc95a6
longfei.liu <longfei.liu@thoughtworks.com>
2019-11-20 11:25:38 +0800

    [liulongfei] feat: realize functional interface

--- src/test/java/com/excersize/javaExcersize/FileProcessTest.java
+++ src/test/java/com/excersize/javaExcersize/FileProcessTest.java
@@ -1,5 +1,6 @@
 package com.excersize.javaExcersize;
 
+import com.excersize.javaExcersize.utils.BufferedReaderProcessor;
 import org.junit.jupiter.api.Test;
 import org.springframework.core.io.ClassPathResource;
 
@@ -15,13 +16,35 @@ class FileProcessTest {
     @Test
     void should_read_first_line() throws IOException {
         String value = processFile();
+        
+        assertEquals("This is first line,", value);
+    }
+    
+    @Test
+    void should_read_two_lines() throws IOException {
+        String value = myProcessFile((BufferedReader br) -> br.readLine() + br.readLine());
+        
+        assertEquals("This is first line,This is second line.", value);
+    }
+    
+    @Test
+    void should_read_first_line_by_lambda() throws IOException {
+        String value = myProcessFile((BufferedReader::readLine));
     
         assertEquals("This is first line,", value);
+    }
+    
+    private String myProcessFile(BufferedReaderProcessor p) throws IOException {
+        File file = new ClassPathResource("test_data.txt").getFile();
+        BufferedReader br = Files.newBufferedReader(file.toPath());
+        
+        return p.process(br);
     }
     
     private String processFile() throws IOException {
         File file = new ClassPathResource("test_data.txt").getFile();
         BufferedReader br = Files.newBufferedReader(file.toPath());
+        
         return br.readLine();
     }
 }
---
+++ src/test/java/com/excersize/javaExcersize/utils/BufferedReaderProcessor.java
@@ -0,0 +1,9 @@
+package com.excersize.javaExcersize.utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+@FunctionalInterface
+public interface BufferedReaderProcessor {
+    String process(BufferedReader b) throws IOException;
+}
